7c6c0c599049f5f7a7dfccca3de4fceb4df1b6d6
new <aji@gmail.com>
2020-08-08 18:31:06 +0100

    make it better

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -79,7 +79,7 @@ module.exports = {
         hot: true,
         overlay: true,
         historyApiFallback: true,
-        watchContentBase: false,
+        watchContentBase: true,
         disableHostCheck: true,
         headers: {
             'Access-Control-Allow-Origin': https ? 'https://0.0.0.0:9999' : 'http://0.0.0.0:9999'
