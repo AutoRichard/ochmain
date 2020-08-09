960f36ebd1c0cf3e9d7e6e9a5358526f27bfbaf8
new <aji@gmail.com>
2020-08-09 13:42:55 +0100

    push

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-var path = require('path');
-var express = require('express');
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = process.env.PORT || 3001;
 
-var app = express();
+app.use('/', express.static(path.join(__dirname)));
 
-app.use(express.static(path.join(__dirname, '/static')));
-app.set('port', process.env.PORT || 8080);
-
-var server = app.listen(app.get('port'), function() {
-  console.log('listening on port ', server.address().port);
-});
+app.listen(port, () => console.log("Listen on PORT", port));
