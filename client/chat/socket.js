d8b42b76a8b1bcba3fa0e5b0dff328edd0662910
new <aji@gmail.com>
2020-08-22 01:58:29 +0100

    chat

--- client/chat/socket.js
+++ client/chat/socket.js
@@ -1,6 +1,6 @@
 import openSocket from 'socket.io-client'
 
-const socket = openSocket('https://ochbackend.herokuapp.com');
+const socket = openSocket('http://localhost:8080');
 
 function showMessage(cb, conversation) {
   socket.emit('show_message', conversation)
