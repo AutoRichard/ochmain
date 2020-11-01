3d5989503e9fcf7445b88197770490a3d7d3b5bf
new <aji@gmail.com>
2020-11-01 22:48:30 +0100

    not

---
+++ client/api/api-post.js
@@ -0,0 +1,45 @@
+//const link = 'http://localhost:8080';
+const link = 'https://ochbackend.herokuapp.com';
+
+const create = (credentials, formData) => {
+    return fetch(link + '/api/post', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + credentials.t
+        },
+        body: formData
+    })
+        .then((response) => {
+            return response.json()
+        }).catch((err) => console.log(err))
+}
+
+
+const list = (userId) => {
+    return fetch(link + '/api/post/' + userId, {
+        method: 'GET',
+    }).then(response => {
+        return response.json()
+    }).catch((err) => console.log(err))
+}
+
+const postComment = (credentials, params) => {
+    return fetch(link + '/api/postComment', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + credentials.t
+        },
+        body: JSON.stringify(params)
+    }).then((response) => {
+        return response.json()
+    }).catch((err) => console.log(err))
+}
+
+export {
+    create,
+    list,
+    postComment
+} 
