ed1f5a1fe0b6d83a1ad006524cd066bd2196151f
new <aji@gmail.com>
2020-12-06 21:32:23 +0100

    ochit

--- client/api/api-user.js
+++ client/api/api-user.js
@@ -1,5 +1,5 @@
-const link = 'http://localhost:8080';
-//const link = 'https://ochback.herokuapp.com';
+//const link = 'http://localhost:8080';
+const link = 'https://ochback.herokuapp.com';
 
 const create = (user) => {
   return fetch(link + '/api/users', {
